39e06b84dacd7e005f8b3672a71fd531fb2c682c
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-19 16:29:23 -0500

    Feat - add input fields to Login form

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 
 const Login = () => {
+    const onChange = e => {
+        console.log(e.target.value);
+    }
+    
     return ( 
-        <h1>From Login</h1>
+        <div className="form-user">
+            <div className="container-form shadow-dark">
+                <h1>Sign in</h1>
+                <form
+
+                >
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your Email"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your password"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <button className="btn btn-primary btn-block">Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
  
