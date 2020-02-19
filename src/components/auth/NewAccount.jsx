e4722d9303cc6ae430c1c5126cb704f90af41ba0
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-19 18:50:14 -0500

    Feat - add sign up form

--- src/components/auth/NewAccount.jsx
+++ src/components/auth/NewAccount.jsx
@@ -1,8 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NewAccount = () => {
+// State for login
+const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm: ''
+    });
+
+    const { name, email, password, confirm } = user;
+
+    const onChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // validate
+        // pass to action (reducer)
+    }
+
     return ( 
-        <h1>From NewAccount</h1>
+        <div className="form-user">
+            <div className="container-form shadow-dark">
+                <h1>Sign Up</h1>
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="field-form">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Your name"
+                            value={name}
+                            onChange={onChange}
+                            autoComplete="none"
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your Email"
+                            value={email}
+                            onChange={onChange}
+                            autoComplete="none"
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your password"
+                            value={password}
+                            onChange={onChange}
+                            autoComplete="none"
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="confirm">Confirm password</label>
+                        <input
+                            type="password"
+                            id="confirm"
+                            name="confirm"
+                            placeholder="Confirm password"
+                            value={confirm}
+                            onChange={onChange}
+                            autoComplete="none"
+                        />
+                    </div>
+                    <div className="field-form">
+                        <button className="btn btn-primary btn-block">Sign Up</button>
+                    </div>
+                </form>
+                <Link to={'/new-account'} className="account-link">
+                    Sign Up
+                </Link>
+            </div>
+        </div>
     );
 }
  
